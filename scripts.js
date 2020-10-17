75225110efca1d146fe78d003e43692454ab6398
lexiamoody <60360405+lexiamoody@users.noreply.github.com>
2020-10-17 11:32:06 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,82 @@
+
+let currentMarker = 'X'
+
+
+
+
+
+const handleClick = (element) => {
+
+
+  console.log(`The element you clicked on has an id:  ${element.id}`)
+
+
+  if(!document.getElementById(element.id).innerHTML){
+    addMarker(element.id)
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+const addMarker = (id) => {
+
+
+  console.log(`*** The current marker is:top-left ${currentMarker}. ***`)
+  console.log(`Therefore, a  "${currentMarker}"  should be placed in the square with the id:  ${id}`)
+  
+
+  document.getElementById(id).innerHTML = currentMarker
+  changeMarker()
+}
+
+
+
+
+
+
+
+
+
+
+
+const changeMarker = () => {
+  if(currentMarker === "X"){
+    currentMarker = "O"
+  } else {
+    currentMarker = "X"
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+const resetBoard = () => {
+  
+
+  const squares = document.getElementsByTagName("TD")
+
+  for (i=0; i < squares.length; i++) {
+
+
+    console.log(squares[i].id)
+
+
+    squares[i].innerHTML = null
+  }  
+}
